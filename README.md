3fe2995f48328c2f5a7b3f1bb4d19182cf250950
Julian Wilkison-Duran <clockworksyler@gmail.com>
2022-03-28 23:44:15 -0400

    this adds more documentation for the future

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # SectorJBackEnd
 this is the API for my indie comic book review site  
+# To generate JWT keys you run the following 
+    ssh-keygen -t rsa -P "" -b 4096 -m PEM -f jwtRS256.key
+# to add the DB user run the following 
+    db.createUser(
+        {
+        user: "julianduranmt",
+        pwd:  "Bmwilk0422!",   // or cleartext password
+        roles: [ { role: "readWrite", db: "sectorJ" } ]
+        }
+    )
--- sector-j-back-end-app /config/database.config.js
+++ sector-j-back-end-app /config/database.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-    url: 'mongodb://julianduranmt:Bmwilk0422!@localhost:27017/easy-notes',
+    url: 'mongodb://julianduranmt:Bmwilk0422!@localhost:27017/sectorJ',
 }
+
+// db.createUser(
+//     {
+//       user: "julianduranmt",
+//       pwd:  "Bmwilk0422!",   // or cleartext password
+//       roles: [ { role: "readWrite", db: "sectorJ" } ]
+//     }
+//   )
+// Successfully added user: {
+//     "user" : "julianduranmt",
+//     "roles" : [
+//     {
+//     "role" : "readWrite",
+//     "db" : "sectorJ"
+//     }
+//     ]
+//     }
