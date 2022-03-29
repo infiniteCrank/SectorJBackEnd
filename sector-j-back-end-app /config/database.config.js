ca830f5a0b1d521e137fdc991e616fdd4ed79e47
Julian Wilkison-Duran <clockworksyler@gmail.com>
2022-03-28 23:44:46 -0400

    removed commented out code

--- sector-j-back-end-app /config/database.config.js
+++ sector-j-back-end-app /config/database.config.js
@@ -1,20 +1,3 @@
 module.exports = {
     url: 'mongodb://julianduranmt:Bmwilk0422!@localhost:27017/sectorJ',
 }
-
-// db.createUser(
-//     {
-//       user: "julianduranmt",
-//       pwd:  "Bmwilk0422!",   // or cleartext password
-//       roles: [ { role: "readWrite", db: "sectorJ" } ]
-//     }
-//   )
-// Successfully added user: {
-//     "user" : "julianduranmt",
-//     "roles" : [
-//     {
-//     "role" : "readWrite",
-//     "db" : "sectorJ"
-//     }
-//     ]
-//     }
