78bd0f3049c301387ecdb01f3a54f46f235badfe
Julian Wilkison-Duran <clockworksyler@gmail.com>
2023-08-09 10:32:41 -0400

    this should fix deprecation warning

--- sector-j-back-end-app/server.js
+++ sector-j-back-end-app/server.js
@@ -44,10 +44,12 @@ app.listen(3000, () => {
 });
 
 mongoose.Promise = global.Promise;
+
 // Connecting to the database
 mongoose.connect(dbConfig.url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: false
 }).then(() => {
     console.log("Successfully connected to the database");    
 }).catch(err => {
