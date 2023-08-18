82868acf090863f61a493a4ebb83e3cea17cf6ff
Debian <bitnami@ip-172-26-3-100.ec2.internal>
2023-08-18 00:51:07 +0000

    run server on 3000

--- sector-j-back-end-app/server.js
+++ sector-j-back-end-app/server.js
@@ -48,7 +48,7 @@ require('./app/routes/note.routes.js')(app);
 require('./app/routes/user.routes.js')(app);
 
 // listen for requests
-app.listen(3001, () => {
+app.listen(3000, () => {
     console.log("Server is listening on port 3000");
 });
 
